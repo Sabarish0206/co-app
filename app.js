c1006e796e9d994979c04e3527e9cf8a65978dd9
Sabarish0206 <sabarioffcl@gmail.com>
2025-01-17 13:32:46 +0530

    create subject route

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
+import subjectRouter from './routes/subjectRoutes.js'
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/subjects",subjectRouter)
+
+
+
+
 app.use("/",(req,res,next)=>{
     res.send("<h1>Hi!!</h1>");
 });
-
 
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
